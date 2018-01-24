d15e7e19b731b3bef6aa9b8081da44ca7bcc3e4f
rock <yazin@whitepayments.com>
2018-01-24 22:26:23 +0300

    Added CRED

---
+++ cred.1m.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# <bitbar.title>Crypto plugin</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Yazin</bitbar.author>
+# <bitbar.author.github>yazinsai</bitbar.author.github>
+# <bitbar.desc>Displays crypto-prices from various exchanges.</bitbar.desc>
+# <bitbar.dependencies>ruby</bitbar.dependencies>
+# <bitbar.abouturl>http://yaz.in/</bitbar.abouturl>
+
+require_relative("./.crypto.rb")
+cred = Crypto.new(:cred, :coinmarketcap, 2)
+
+puts "#{cred.refresh} | templateImage=#{cred.logo}"
